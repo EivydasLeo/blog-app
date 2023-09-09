ed45d49f734e623f5166d27cdd868dcc89d34c37
EivydasLeo <eivydasleo@gmail.com>
2023-09-09 10:12:54 +0300

    DarkModeToggle implementation.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import Link from "next/link";
 import { links } from "@/utils/dummyData/navbar";
 import styles from './navbar.module.css'
+import DarkModeToggle from "@/components/DarkModeToggle/DarkModeToggle";
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <Link className={styles.logo} href='/'>HomePage</Link>
             <div className={styles.links}>
+                <DarkModeToggle />
                 {links.map(link => (
                     <Link className={styles.link} key={link.id} href={link.url}>
                         {link.title}
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { createContext, useState, ReactNode } from "react";
+
+// Define the shape of the context
+interface ThemeContextType {
+    mode: string;
+    toggle: () => void;
+}
+
+// Create the context with a default value
+export const ThemeContext = createContext<ThemeContextType>({
+    mode: "light",
+    toggle: () => {}
+});
+
+// Define the props for the ThemeProvider
+interface ThemeProviderProps {
+    children: ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+    const [mode, setMode] = useState<string>("light");
+
+    const toggle = () => {
+        setMode((prev) => (prev === "dark" ? "light" : "dark"));
+    }
+
+    return (
+    <ThemeContext.Provider value={{ toggle, mode }}>
+        <div className={`theme ${mode}`}>
+            {children}
+        </div>
+    </ThemeContext.Provider>
+);
+};

ac3378d4eb5fea10f5c126f6cab181f9e5965b43
Eivydas <eivydas.leonavicius@nfq.com>
2023-10-09 20:31:28 +0300

    reseted changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   <div className={styles.container}>
     <div className={styles.item}>
       <h1 className={styles.title}>Better design for your digital products.</h1>
-      <p className={styles.desc}>Turning your Idea into Reality. We bring together the teams from the global tech industry.</p>
+      <p className={styles.desc}>Turning your Idea into Reality. We bring together the teams from the global tech industry .</p>
       <Button text="See Our Works" url="/portfolio" />
     </div>
     <div className={styles.item}>

61a6e2624233dc5dfb4ce4cb16171498fe3c28c8
EivydasLeo <eivydasleo@gmail.com>
2024-01-22 11:05:37 +0200

    Portfolio section and data.

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -2,60 +2,41 @@ import React from "react";
 import styles from "./page.module.scss";
 import Button from "@/components/Button/Button";
 import Image from "next/image";
+import { items } from "./data";
+import { notFound } from "next/navigation";
 
 interface MatchParams {
-    category: string; // Define category as a property of params
+    category: string;
 }
 
+const getData = (cat) => {
+    const data = items[cat];
+
+    if (data) {
+        return data;
+    }
+
+    return notFound();
+};
+
 const Category: React.FC<{ params: MatchParams }> = ({ params }) => {
+    const data = getData(params.category);
     return (
         <div className={styles.container}>
             <h1 className={styles.catTitle}>{params.category}</h1>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.desc}>Desc</p>
-                    <Button text="See More" url="#" />
+
+            {data.map((item) => (
+                <div className={styles.item} key={item.id}>
+                    <div className={styles.content}>
+                        <h1 className={styles.title}>{item.title}</h1>
+                        <p className={styles.desc}>{item.desc}</p>
+                        <Button text="See More" url="#" />
+                    </div>
+                    <div className={styles.imgContainer}>
+                        <Image className={styles.img} fill={true} src={item.image} alt="" />
+                    </div>
                 </div>
-                <div className={styles.imgContainer}>
-                    <Image
-                        className={styles.img}
-                        fill={true}
-                        src="https://picsum.photos/1600/400"
-                        alt=""
-                    />
-                </div>
-            </div>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.desc}>Desc</p>
-                    <Button text="See More" url="#" />
-                </div>
-                <div className={styles.imgContainer}>
-                    <Image
-                        className={styles.img}
-                        fill={true}
-                        src="https://picsum.photos/1600/400"
-                        alt=""
-                    />
-                </div>
-            </div>
-            <div className={styles.item}>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>Test</h1>
-                    <p className={styles.desc}>Desc</p>
-                    <Button text="See More" url="#" />
-                </div>
-                <div className={styles.imgContainer}>
-                    <Image
-                        className={styles.img}
-                        fill={true}
-                        src="https://picsum.photos/1600/400"
-                        alt=""
-                    />
-                </div>
-            </div>
+            ))}
         </div>
     );
 };

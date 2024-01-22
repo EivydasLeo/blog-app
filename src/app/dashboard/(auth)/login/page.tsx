c2d23ca18cf883ab4d0b0c922742ff50ef67a9c5
EivydasLeo <eivydasleo@gmail.com>
2024-01-22 11:15:39 +0200

    Updated login page.tsx and sccs file.

--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,7 +1,40 @@
+"use client";
 import React from "react";
+import styles from "./page.module.scss";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-function Login() {
-    return <div>Login</div>;
-}
+const Login = () => {
+    const session = useSession();
+    const router = useRouter();
+
+    if (session.status === "loading") {
+        return <p>Loading...</p>;
+    }
+
+    if (session.status === "authenticated") {
+        router?.push("/dashboard");
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        signIn("credentials", { email, password });
+    };
+
+    return (
+        <div className={styles.container}>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <input type="text" placeholder="Email" required className={styles.input} />
+                <input type="password" placeholder="Password" required className={styles.input} />
+                <button className={styles.button}>Login</button>
+            </form>
+            <button onClick={async () => await signIn("google")}>Login with Google</button>
+        </div>
+    );
+};
 
 export default Login;

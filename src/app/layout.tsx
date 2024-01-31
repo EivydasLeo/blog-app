9f790fa733d6ec09bfea5188e97a2866a4948d2d
EivydasLeo <eivydasleo@gmail.com>
2024-01-31 18:27:22 +0200

    Updated layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
-import "../scss/global/globals.scss";
-import { Inter, Roboto_Mono } from "next/font/google";
+import "@/scss/global.scss";
 import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer/footer";
 import { ThemeProvider } from "@/context/themeContext";
 import AuthProvider from "@/components/authProvider/authProvider";
-
-export const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
- 
-export const roboto_mono = Roboto_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export const metadata = {
     title: "Blog App",
@@ -23,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body>
                 <ThemeProvider>
                     <AuthProvider>
                         <div className="container">

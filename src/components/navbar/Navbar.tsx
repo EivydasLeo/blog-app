1b77e2ca6ce4fe342a2cf1cbcd26cf15febf12ee
EivydasLeo <eivydasleo@gmail.com>
2024-02-06 17:39:56 +0200

    Navbar component optimize

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,11 @@
 "use client";
 import React from "react";
 import styles from "@/scss/components/navbar/navbar.module.scss";
-import Link from "next/link";
-import { links } from "@/utils/dummyData/navbar";
 import { signOut, useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
+import Links from "../Links/Links";
 
 const Navbar: React.FC = () => {
     const { status } = useSession();
-    const pathname = usePathname();
-
-    const handleLinkActive = (path: string): string => (path === pathname ? styles.active : "");
 
     const handleSignOut = async (): Promise<void> => {
         try {
@@ -22,30 +17,17 @@ const Navbar: React.FC = () => {
 
     return (
         <div className={styles.container}>
-            <Link href="/" className={styles.logo}>
-                SKA
-            </Link>
-            <div className={styles.links}>
-                {links.map((link) => (
-                    <Link
-                        key={link.id}
-                        href={link.url}
-                        className={`${styles.link} ${handleLinkActive(link.url)}`}
-                    >
-                        {link.title}
-                    </Link>
-                ))}
-                {status === "authenticated" && (
-                    <button
-                        className={styles.logout}
-                        onClick={() => {
-                            void handleSignOut();
-                        }}
-                    >
-                        Logout
-                    </button>
-                )}
-            </div>
+            <Links isAuthenticated={status === "authenticated"} />
+            {status === "authenticated" && (
+                <button
+                    className={styles.logout}
+                    onClick={() => {
+                        void handleSignOut();
+                    }}
+                >
+                    Logout
+                </button>
+            )}
         </div>
     );
 };

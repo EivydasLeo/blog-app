073701a63ba108b0e3167f5d2954ae744c265b45
EivydasLeo <eivydasleo@gmail.com>
2024-02-07 18:06:47 +0200

    Links update

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import styles from "@/scss/components/links/links.module.scss";
-import { links } from "@/utils/dummyData/navbar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { logo } from "@/utils/dummyData/data";
+import { LogoData, linksData } from "@/utils/dummyData/data";
 
 interface LinksProps {
-    isFooter?: boolean;
+    isFooter: boolean;
     isAuthenticated: boolean;
 }
 
-const Links: React.FC<LinksProps> = ({ isFooter = false, isAuthenticated }) => {
+export const Links: React.FC<LinksProps> = ({ isFooter, isAuthenticated }) => {
+    const { headerLogo } = LogoData;
     const pathname = usePathname();
 
     const handleLinkActive = (path: string): string => (path === pathname ? styles.active : "");
 
     const filteredLinks = isAuthenticated
-        ? links
-        : links.filter((link) => link.isHidden === undefined || !link.isHidden);
+        ? linksData
+        : linksData.filter((link) => link.isHidden === undefined || !link.isHidden);
 
     return (
         <div className={isFooter ? styles.footer_container : styles.container}>
             <Link href="/" className={styles.logo}>
-                {logo.header_logo}
+                {headerLogo}
             </Link>
+
             <div className={isFooter ? styles.footer_links : styles.links}>
                 {filteredLinks.map(({ id, url, title }) => (
                     <Link
@@ -31,12 +32,10 @@ const Links: React.FC<LinksProps> = ({ isFooter = false, isAuthenticated }) => {
                         href={url}
                         className={`${isFooter ? styles.footer_link : styles.link} ${handleLinkActive(url)}`}
                     >
-                        {title}
+                        {isFooter ? title : title.toUpperCase()}
                     </Link>
                 ))}
             </div>
         </div>
     );
 };
-
-export default Links;

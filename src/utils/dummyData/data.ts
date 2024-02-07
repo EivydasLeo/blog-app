528f0ead2c3ebcc159b499d7ac09a1ef14078f75
EivydasLeo <eivydasleo@gmail.com>
2024-02-07 17:59:17 +0200

    dummyData refactor

--- src/utils/dummyData/data.ts
+++ src/utils/dummyData/data.ts
@@ -1,72 +1,14 @@
-export const items = {
-    applications: [
-        {
-            id: 1,
-            title: "Creative Portfolio",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
-        },
-        {
-            id: 2,
-            title: "Minimal Single Product",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
-        },
-        {
-            id: 3,
-            title: "Strong Together Charity",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
-        },
-    ],
-    illustrations: [
-        {
-            id: 1,
-            title: "Creative Portfolio",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
-        },
-        {
-            id: 2,
-            title: "Minimal Single Product",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
-        },
-        {
-            id: 3,
-            title: "Strong Together Charity",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
-        },
-    ],
-    websites: [
-        {
-            id: 1,
-            title: "Creative Portfolio",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
-        },
-        {
-            id: 2,
-            title: "Minimal Single Product",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
-        },
-        {
-            id: 3,
-            title: "Strong Together Charity",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
-            image: "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
-        },
-    ],
+export const LogoData = {
+    headerLogo: "SKA",
 };
 
-export const logo = {
-    header_logo: "SKA",
-};
-
-export const socialLinks = [
+export const SocialLinksData = [
     { src: "/icons/facebook.png", alt: "Facebook Account", href: "https://www.facebook.com/" },
     { src: "/icons/instagram.png", alt: "Instagram Account", href: "https://www.instagram.com/" },
     { src: "/icons/tiktok.png", alt: "TikTok Account", href: "https://www.tiktok.com/" },
 ];
+
+export const CopyrightData = {
+    copyrightDate: `© ${new Date().getFullYear()} Food Blogger & Influencer`,
+    copyrightText: "Powered by Food Blogger & Influencer",
+};

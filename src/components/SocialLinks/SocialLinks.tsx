ae28fdec325a5ba332594347d473d94b9928450d
EivydasLeo <eivydasleo@gmail.com>
2024-02-07 18:07:16 +0200

    SocialLinks update

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import styles from "@/scss/components/SocialLinks/social_links.module.scss";
-import { socialLinks } from "@/utils/dummyData/data";
+import { SocialLinksData } from "@/utils/dummyData/data";
 import Link from "next/link";
 
-const SocialLinks: React.FC = () => {
+export const SocialLinks: React.FC = () => {
     return (
-        <div className={styles.social_container}>
-            {socialLinks.map(({ src, alt, href }, index) => (
-                <Link className={styles.social_link} key={index} href={href}>
-                    <Image
-                        className={styles.social_icon}
-                        src={src}
-                        alt={alt}
-                        fill={true}
-                        loading="lazy"
-                    />
+        <div className={styles.container}>
+            {SocialLinksData.map(({ src, alt, href }, index) => (
+                <Link className={styles.link} key={index} href={href}>
+                    <Image className={styles.icon} src={src} alt={alt} fill={true} loading="lazy" />
                 </Link>
             ))}
         </div>
     );
 };
-
-export default SocialLinks;

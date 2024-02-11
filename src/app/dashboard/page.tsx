6bf9985e13293821391aef2f3f5d9eba43fdc6f2
EivydasLeo <eivydasleo@gmail.com>
2024-02-11 12:23:01 +0200

    Refactored dashboard page by Ts,Eslint rules.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,10 +6,17 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
-const Dashboard = () => {
-    const session = useSession();
+interface Post {
+    _id: string;
+    title: string;
+    image: string;
+}
 
+const Dashboard: React.FC = () => {
+    const session = useSession();
     const router = useRouter();
+
+    console.log(session, "session");
 
     useEffect(() => {
         if (session.status === "unauthenticated") {
@@ -17,94 +24,96 @@ const Dashboard = () => {
         }
     }, [session.status, router]);
 
-    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+    const fetcher = async (url: RequestInfo, init?: RequestInit): Promise<any> => {
+        const res = await fetch(url, init);
+        return await res.json();
+    };
 
-    const { data, mutate, error, isLoading } = useSWR(
-        `/api/posts?username=${session?.data?.user.name}`,
+    const { data, mutate, isLoading } = useSWR(
+        `/api/posts?username=${session?.data?.user?.name}`,
         fetcher,
     );
 
-    if (session.status === "loading") {
-        return <p>Loading...</p>;
-    }
-
-    if (session.status === "unauthenticated") {
-        router?.push("/dashboard/login");
-    }
-
-    const handleSubmit = async (e) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
         e.preventDefault();
-        const title = e.target[0].value;
-        const desc = e.target[1].value;
-        const image = e.target[2].value;
-        const content = e.target[3].value;
-
+        const formElements = (e.target as HTMLFormElement).elements;
+        const [title, desc, image, content] = Array.from(formElements) as HTMLInputElement[];
         try {
             await fetch("/api/posts", {
                 method: "POST",
                 body: JSON.stringify({
-                    title,
-                    desc,
-                    image,
-                    content,
-                    username: session.data.user.name,
+                    title: title.value,
+                    desc: desc.value,
+                    image: image.value,
+                    content: content.value,
+                    username: session?.data?.user?.name,
                 }),
             });
-            mutate();
-            e.target.reset();
+            void mutate();
+            (e.target as HTMLFormElement).reset();
         } catch (err) {
-            console.log(err);
+            console.error(err);
         }
     };
 
-    const handleDelete = async (id) => {
+    const handleDelete = async (id: string): Promise<void> => {
         try {
             await fetch(`/api/posts/${id}`, {
                 method: "DELETE",
             });
-            mutate();
+            void mutate();
         } catch (err) {
-            console.log(err);
+            console.error(err);
         }
     };
 
-    if (session.status === "authenticated") {
-        return (
-            <div className={styles.container}>
-                <div className={styles.posts}>
-                    {isLoading
-                        ? "loading"
-                        : data?.map((post) => (
-                              <div className={styles.post} key={post._id}>
-                                  <div className={styles.imgContainer}>
-                                      <Image src={post.image} alt="" width={200} height={100} />
+    return (
+        <div className={styles.container}>
+            {session.status === "loading" && <p>Loading...</p>}
+            {session.status === "authenticated" && (
+                <>
+                    <div className={styles.posts}>
+                        {isLoading
+                            ? "loading"
+                            : data?.map((post: Post) => (
+                                  <div className={styles.post} key={post._id}>
+                                      <div className={styles.imgContainer}>
+                                          <Image src={post.image} alt="" width={200} height={100} />
+                                      </div>
+                                      <h2 className={styles.postTitle}>{post.title}</h2>
+                                      <span
+                                          className={styles.delete}
+                                          onClick={() => {
+                                              void handleDelete(post._id);
+                                          }}
+                                      >
+                                          X
+                                      </span>
                                   </div>
-                                  <h2 className={styles.postTitle}>{post.title}</h2>
-                                  <span
-                                      className={styles.delete}
-                                      onClick={() => handleDelete(post._id)}
-                                  >
-                                      X
-                                  </span>
-                              </div>
-                          ))}
-                </div>
-                <form className={styles.new} onSubmit={handleSubmit}>
-                    <h1>Add New Post</h1>
-                    <input type="text" placeholder="Title" className={styles.input} />
-                    <input type="text" placeholder="Desc" className={styles.input} />
-                    <input type="text" placeholder="Image" className={styles.input} />
-                    <textarea
-                        placeholder="Content"
-                        className={styles.textArea}
-                        cols="30"
-                        rows="10"
-                    ></textarea>
-                    <button className={styles.button}>Send</button>
-                </form>
-            </div>
-        );
-    }
+                              ))}
+                    </div>
+                    <form
+                        className={styles.new}
+                        onSubmit={(e) => {
+                            void handleSubmit(e);
+                        }}
+                    >
+                        <h1>Add New Post</h1>
+                        <input type="text" placeholder="Title" className={styles.input} />
+                        <input type="text" placeholder="Desc" className={styles.input} />
+                        <input type="text" placeholder="Image" className={styles.input} />
+                        <textarea
+                            placeholder="Content"
+                            className={styles.textArea}
+                            cols={30}
+                            rows={10}
+                        ></textarea>
+                        <button className={styles.button}>Send</button>
+                    </form>
+                </>
+            )}
+        </div>
+    );
 };
 
 export default Dashboard;

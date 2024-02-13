5cbbdc36267a814ed1f8e91f25b6cbb55a0ac09c
EivydasLeo <eivydasleo@gmail.com>
2024-02-13 17:12:53 +0200

    Replaced Button component to ButtonLink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "@/scss/app/page.module.scss";
-import { Button } from "@/components/Button/Button";
+import Image from "next/image";
 import { ArticleCard } from "@/components/ArticleCard/ArticleCard";
 import { PersonData } from "@/utils/dummyData/data";
 import { Divider } from "@/components/Divider/Divider";
-import Image from "next/image";
+import { ButtonLink } from "@/components/ButtonLink/ButtonLink";
 
 const Home: React.FC = (): React.ReactNode => {
     const { title, subtitle, text } = PersonData;
@@ -13,7 +13,7 @@ const Home: React.FC = (): React.ReactNode => {
             <div className={styles.item}>
                 <Divider text="Welcome" />
                 <ArticleCard title={title} subtitle={subtitle} text={text} />
-                <Button text="Join insider" url="/dashboard/login" />
+                <ButtonLink text="Join insider" url="/dashboard/login" />
             </div>
             <Image src="/image/blogger.jpg" alt="blogger" width={550} height={550} />
         </div>

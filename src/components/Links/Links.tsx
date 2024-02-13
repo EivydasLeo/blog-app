e6f8a1110c702761321841988c4c037a89bc6f56
EivydasLeo <eivydasleo@gmail.com>
2024-02-13 16:51:42 +0200

    Links component update

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -3,13 +3,9 @@ import styles from "@/scss/components/links/links.module.scss";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { LogoData, linksData } from "@/utils/dummyData/data";
+import { type LinksProps } from "@/utils/dummyData/props";
 
-interface LinksProps {
-    isFooter: boolean;
-    isAuthenticated: boolean;
-}
-
-export const Links: React.FC<LinksProps> = ({ isFooter, isAuthenticated }) => {
+export const Links: React.FC<LinksProps> = ({ isFooter = false, isAuthenticated }) => {
     const { headerLogo } = LogoData;
     const pathname = usePathname();
 
@@ -20,24 +16,31 @@ export const Links: React.FC<LinksProps> = ({ isFooter, isAuthenticated }) => {
         : linksData.filter((link) => link.isHidden === undefined || !link.isHidden);
 
     return (
-        <div className={isFooter ? styles.footer_container : styles.container}>
-            {!isFooter && (
-                <Link href="/" className={styles.logo}>
-                    {headerLogo}
-                </Link>
-            )}
-
-            <div className={isFooter ? styles.footer_links : styles.links}>
-                {filteredLinks.map(({ id, url, title }) => (
-                    <Link
-                        key={id}
-                        href={url}
-                        className={`${isFooter ? styles.footer_link : styles.link} ${handleLinkActive(url)}`}
-                    >
-                        {isFooter ? title : title.toUpperCase()}
+        <>
+            {!isFooter ? (
+                <div className={styles.container}>
+                    <Link href="/" className={styles.logo}>
+                        {headerLogo}
                     </Link>
-                ))}
-            </div>
-        </div>
+                    <div className={styles.links}>
+                        {filteredLinks.map(({ id, url, title }) => (
+                            <Link
+                                key={id}
+                                href={url}
+                                className={`${styles.link} ${handleLinkActive(url)}`}
+                            >
+                                {title.toUpperCase()}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                filteredLinks.map(({ id, url, title }) => (
+                    <Link key={id} href={url} className={`${styles.link} ${handleLinkActive(url)}`}>
+                        {title}
+                    </Link>
+                ))
+            )}
+        </>
     );
 };

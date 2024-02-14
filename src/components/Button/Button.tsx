66a46487e3abc42f547edade98c0dbb21262aa47
EivydasLeo <eivydasleo@gmail.com>
2024-02-14 16:30:27 +0200

    Button component refactor

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,14 +4,14 @@ import styles from "@/scss/components/button/button.module.scss";
 interface ButtonProps {
     text: React.ReactNode;
     handleClick?: () => void;
-    variant?: "small" | "large";
+    variant?: "small" | "";
     title: string;
     disabled?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({ text, handleClick, variant, title, disabled }) => {
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    const classNames = `${variant ? styles[variant] : styles.container}`;
+    const classNames = `${styles.btn} ${variant ? styles[variant] : ""}`;
 
     return (
         <button className={classNames} onClick={handleClick} title={title} disabled={disabled}>

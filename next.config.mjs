47aaab62fd2a49f958764b834e37d38aef24c544
EivydasLeo <eivydasleo@gmail.com>
2024-09-22 13:16:10 +0300

    Added sass prependData config so all components will reach global variables.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    prependData: `
+        @import "/app/scss/variables/variables.scss";
+      `,
+  },
+};
 
 export default nextConfig;

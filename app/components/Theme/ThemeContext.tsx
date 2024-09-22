99d7753335e0e3e71253a0270a0345e5cd3f958f
EivydasLeo <eivydasleo@gmail.com>
2024-09-22 15:12:50 +0300

    Created ThemeContext component

---
+++ app/components/Theme/ThemeContext.tsx
@@ -0,0 +1,10 @@
+import React, { type ReactNode } from "react";
+import styles from "@/app/scss/components/Theme/theme.module.scss";
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  return <div className={styles.light}>{children}</div>;
+};

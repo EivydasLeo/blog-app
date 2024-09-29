36ad92b1df5dbbb9d158554cef33c4da262d4dbd
EivydasLeo <eivydasleo@gmail.com>
2024-09-29 14:02:16 +0300

    Imported schemaTypes into index.ts

--- types/schemaTypes/index.ts
+++ types/schemaTypes/index.ts
@@ -1,3 +1,6 @@
 import author from "./author";
+import about from "./about";
+import photo from "./photo";
+import slides from "./slides";
 
-export const schemaTypes = [author];
+export const schemaTypes = [author, about, photo, slides];

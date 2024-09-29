b567ffaccca0930f221e580ae06677231e9fec59
EivydasLeo <eivydasleo@gmail.com>
2024-09-29 14:02:04 +0300

    Created about schemaType

---
+++ types/schemaTypes/about.ts
@@ -0,0 +1,64 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "about",
+  title: "About",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: "headline",
+      title: "Headline",
+      type: "string",
+      description: "In one short sentence, what do you do?",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "profileImage",
+      title: "Profile Image",
+      type: "image",
+      description: "Upload a profile picture",
+      options: { hotspot: true },
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    }),
+    defineField({
+      name: "bio",
+      title: "Bio",
+      type: "array",
+      of: [
+        {
+          title: "Block",
+          type: "block",
+          styles: [{ title: "Normal", value: "normal" }],
+          lists: [],
+        },
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+      media: "image",
+    },
+  },
+});

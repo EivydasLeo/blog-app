6b17589cf0bb7a157e26d1eae14d729308045059
EivydasLeo <eivydasleo@gmail.com>
2024-09-29 14:00:45 +0300

    Created photo, slides schemaTypes

---
+++ types/schemaTypes/slides.ts
@@ -0,0 +1,27 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  type: "document",
+  name: "slides",
+  title: "Slides",
+  fields: [
+    defineField({
+      name: "slides",
+      type: "array",
+      title: "Slides",
+      of: [
+        defineField({
+          type: "reference",
+          to: [{ type: "photo" }],
+          name: "slideReference",
+        }),
+      ],
+    }),
+  ],
+  preview: {
+    select: {
+      title: "slides.0.title",
+      media: "slides.0.image",
+    },
+  },
+});

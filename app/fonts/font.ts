790fe02a972810a53eb752502809caef3e1330db
EivydasLeo <eivydasleo@gmail.com>
2024-10-19 15:44:34 +0300

    Added localFont

--- app/fonts/font.ts
+++ app/fonts/font.ts
@@ -1,4 +1,5 @@
 import { Montserrat, Fraunces } from "next/font/google";
+import localFont from "next/font/local";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
@@ -10,4 +11,10 @@ export const fraunces = Fraunces({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-fraunces",
+});
+
+export const rheinmetall = localFont({
+  src: "../../public/fonts/1952 RHEINMETALL.woff2",
+  display: "swap",
+  variable: "--font-rheinmetall",
 });

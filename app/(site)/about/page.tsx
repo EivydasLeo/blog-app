cbfe1de03111481d1d638e2a2b988b725c6e6798
EivydasLeo <eivydasleo@gmail.com>
2024-10-20 13:39:11 +0300

    Implemented about page with sanity schemaType, query and types.

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,12 +1,45 @@
 import React from "react";
 import styles from "@/app/styles/about/about.module.scss";
+import { Divider } from "@/app/components/Divider/Divider";
+import { getSlide } from "@/sanity/sanity.query";
+import { SlideType } from "@/types/types";
+import ProfileSlider from "@/app/components/ProfileSlider/ProfileSlider";
+import { getAbout } from "@/sanity/sanity.query";
+import type { AboutType } from "@/types/types";
+import { PortableText } from "@portabletext/react";
+import Link from "next/link";
 
-const About: React.FC = () => {
+export default async function About() {
+  const slides: SlideType[] = await getSlide();
+  const about: AboutType[] = await getAbout();
+
   return (
     <div className={styles.container}>
-      About the author with an image and at the bottom, at the footer, an author
-      image gallery with a slider
+      <div className={styles.item}>
+        <Divider text="About Author" />
+        {about &&
+          about.map((data) => (
+            <div key={data._id} className={styles.bio}>
+              <PortableText value={data.bio} />
+              <PortableText value={data.writing} />
+              {data.books.map((book) => (
+                <Link key={book._key} href={book.link}>
+                  {book.title}
+                </Link>
+              ))}
+              {data.articles.map((article) => (
+                <Link key={article._key} href={article.link}>
+                  {article.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+      </div>
+      {slides.map((data) => (
+        <div className={styles.slider} key={data._id}>
+          <ProfileSlider slides={data.slides} _id={data._id} />
+        </div>
+      ))}
     </div>
   );
-};
-export default About;
+}

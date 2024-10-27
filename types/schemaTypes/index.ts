a93b7351ab17a4fea30a52dd6bf2ad0e325a0d37
EivydasLeo <eivydasleo@gmail.com>
2024-10-27 12:24:26 +0200

    Updated index.ts file: Added localeString and localeBlockContent types

--- types/schemaTypes/index.ts
+++ types/schemaTypes/index.ts
@@ -4,5 +4,16 @@ import photo from "./photo";
 import slides from "./slides";
 import stories from "./stories";
 import books from "./books";
+import localeString from "../locales/localizationSchemaTypes/localeString";
+import localeBlockContent from "../locales/localizationSchemaTypes/localeBlockContent";
 
-export const schemaTypes = [author, about, photo, slides, stories, books];
+export const schemaTypes = [
+  author,
+  about,
+  photo,
+  slides,
+  stories,
+  books,
+  localeString,
+  localeBlockContent,
+];

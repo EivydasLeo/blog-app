48c8afd550671d7f056f95f96e3a73d43dc1aa96
EivydasLeo <eivydasleo@gmail.com>
2024-11-10 12:34:15 +0200

    Updated Footer file: Changed html structure

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import styles from "@/app/styles/components/footer/footer.module.scss";
-import { Links } from "../Links/Links";
+import { NavigationLinks } from "../NavigationLinks/NavigationLinks";
 import { SocialLinks } from "../SocialLinks/SocialLinks";
 import { Copyright } from "../Copyright/Copyright";
 
 export const Footer: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <footer className={styles.container}>
       <div className={styles.body}>
-        <div className={styles.links}>
-          <Links isFooter />
-        </div>
+        <ul className={styles.links}>
+          <NavigationLinks isFooter />
+        </ul>
         <div className={styles.social}>
           <SocialLinks />
         </div>
       </div>
       <Copyright />
-    </div>
+    </footer>
   );
 };

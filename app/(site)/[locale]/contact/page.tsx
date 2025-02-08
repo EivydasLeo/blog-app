f26cec924c68c8ac655f06abd408139f4474cb6c
EivydasLeo <eivydasleo@gmail.com>
2025-02-08 17:32:32 +0200

    Removed headerLogo from NavigationLinks file.  Added styles to contact page.

--- app/(site)/[locale]/contact/page.tsx
+++ app/(site)/[locale]/contact/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getTranslations } from "next-intl/server";
 import ContactForm from "@/app/components/ContactForm/ContactForm";
 import { Divider } from "@/app/components/Divider/Divider";
+import styles from "@/app/styles/contact/contact.module.scss";
 
 export const metadata = {
   title: "Contact",
@@ -33,7 +34,7 @@ export default async function ContactPage({
   };
 
   return (
-    <div>
+    <div className={styles.container}>
       <Divider text={translations.divider} />
       <ContactForm locale={params.locale} translations={translations} />
     </div>
--- app/components/NavigationLinks/NavigationLinks.tsx
+++ app/components/NavigationLinks/NavigationLinks.tsx
@@ -3,12 +3,10 @@ import React from "react";
 import styles from "@/app/styles/components/links/links.module.scss";
 import { Link } from "@/i18n/routing";
 import { usePathname } from "next/navigation";
-import { LogoData } from "../../utils/dummyData/data";
 import { type LinksProps } from "../../utils/dummyData/props";
 import { useTranslations } from "next-intl";
 
 export const NavigationLinks: React.FC<LinksProps> = ({ isFooter = false }) => {
-  const { headerLogo } = LogoData;
   const pathname = usePathname();
   const t = useTranslations("HomePage");
   const navigationKeys = Object.keys(t.raw("navigation"));
@@ -22,9 +20,6 @@ export const NavigationLinks: React.FC<LinksProps> = ({ isFooter = false }) => {
     <>
       {!isFooter ? (
         <ul className={styles.container}>
-          <Link href={"/"} className={styles.logo}>
-            {headerLogo}
-          </Link>
           {navigationKeys.map((key) => (
             <li className={styles.links} key={key}>
               <Link
